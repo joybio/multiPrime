c479d9319cb49984789b75187c5ea6ab95f7df80
joybio <yang_junbo_hi@126.com>
2023-02-21 13:33:40 +0800

    update

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bsh
-snakemake --configfile multiPrime.yaml -s multiPrime.py --cores 30
+#snakemake --configfile multiPrime.yaml -s multiPrime.py --cores 30
+
+snakemake --configfile multiPrime2.yaml -s multiPrime2.py --cores 30
 
 
--- snakemake_dag.sh
+++ snakemake_dag.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+# Note: Checkpoints result in inaccurate flow diagrams.
+
 snakemake --configfile multiPrime.yaml -s multiPrime.py --dag | dot -Tpdf > dag.pdf
+
+snakemake --configfile multiPrime2.yaml -s multiPrime2.py --dag | dot -Tpdf > dag.pdf
 
 #snakemake --dag report.html | dot -Tsvg > dag.svg
 #snakemake -s *** --cores 10
