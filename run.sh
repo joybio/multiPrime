3dded3055de81b8053e2c0a47c6a156cbaa12efe
joybio <yang_junbo_hi@126.com>
2023-08-03 17:16:39 +0800

    update

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bsh
-#snakemake --configfile multimulti-DegePrime.yaml -s multi-DegePrime.py --cores 10 --resources mem_mb=80000
+#snakemake --configfile multi-DegePrime.yaml -s multi-DegePrime.py --cores 10 --resources mem_mb=80000
 
-#snakemake --configfile multiPrime-orinal.yaml -s multiPrime-orignal.py --cores 10 --resources mem_mb=80000
+#snakemake --configfile multiPrime-original.yaml -s multiPrime-original.py --cores 10 --resources mem_mb=80000
 
 snakemake --configfile multiPrime.yaml -s multiPrime.py --cores 10 --resources mem_mb=80000
 
